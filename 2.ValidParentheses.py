fb8b490fbaa5900b7d64f7ff21122114118d5952
AbhiChavan02 <abhijeetchavan212002@gmail.com>
2026-01-11 23:26:13 +0530

    Check Valid Parentheses problem solved

---
+++ 2.ValidParentheses.py
@@ -0,0 +1,34 @@
+from turtle import reset
+
+
+class Solution:
+    def isValid(self, s:str) -> bool:
+        stack = []
+
+        bracket_map = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+
+        for ch in s:
+            if ch in bracket_map:
+                top = stack.pop() if stack else '#'
+                if bracket_map[ch] != top:
+                    return False
+            else:
+                stack.append(ch)
+
+        return not stack
+    
+if __name__ == "__main__":
+    s = input("Enter parentheses string: ").strip()
+
+    solution = Solution()
+    result = solution.isValid(s)
+
+    print("Valid Parentheses: ", result)
+
+
+    # ([]) = True 
+    # ([)) = False
