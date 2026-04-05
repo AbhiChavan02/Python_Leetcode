0e7b7e8872f6288566fed7ad07c20aff248c8330
AbhiChavan02 <abhijeetchavan212002@gmail.com>
2026-04-05 22:43:16 +0530

    Solved Robot Return to Origin problem on leetcode

---
+++ 84.RobotReturnOrigin.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        x = y = 0
+        for move in moves:
+            if move == 'U':
+                y -= 1
+            elif move == 'D':
+                y += 1
+            elif move == 'L':
+                x -= 1
+            elif move == 'R':
+                x += 1
+
+        return x == 0 and y == 0
+
+
+if __name__ == "__main__":
+    moves = input("Enter moves (U/D/L/R): ")
+    sol = Solution()
+    result = sol.judgeCircle(moves)
+    print(result)
+
+    # UDLR
+    # -> True
+
+    # UDLP
+    # -> False
