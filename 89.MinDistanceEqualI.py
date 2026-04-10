eafbd78163ce581c1d82df932177bb9228f392ad
AbhiChavan02 <abhijeetchavan212002@gmail.com>
2026-04-10 17:58:00 +0530

    Solved Minimum Distance between Three Equal Elements I on leetcode

---
+++ 89.MinDistanceEqualI.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = n + 1
+
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                if nums[i] != nums[j]:
+                    continue
+                for k in range(j + 1, n):
+                    if nums[j] == nums[k]:
+                        ans = min(ans, k - i)
+                        break
+
+        return -1 if ans == n + 1 else ans * 2
+
+
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+
+obj = Solution()
+result = obj.minimumDistance(nums)
+
+print("Output:", result)
+
+
+# 1 2 1 3 1
+# -> 4
