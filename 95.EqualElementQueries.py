bc524b953d45ed41db1a8f08532e7a2011cdb746
AbhiChavan02 <abhijeetchavan212002@gmail.com>
2026-04-16 17:55:07 +0530

    Solved Closest Equal Element Queries on Leetcode

---
+++ 95.EqualElementQueries.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+import bisect
+
+class Solution:
+    def solveQueries(self, nums, queries):
+        n = len(nums)
+        nums_pos = defaultdict(list)
+
+        for i in range(n):
+            nums_pos[nums[i]].append(i)
+
+        for pos in nums_pos.values():
+            x = pos[0]
+            pos.insert(0, pos[-1] - n)
+            pos.append(x + n)
+
+        for i in range(len(queries)):
+            x = nums[queries[i]]
+            pos_list = nums_pos[x]
+            if len(pos_list) == 3:
+                queries[i] = -1
+                continue
+            pos = bisect.bisect_left(pos_list, queries[i])
+            queries[i] = min(
+                pos_list[pos + 1] - pos_list[pos],
+                pos_list[pos] - pos_list[pos - 1],
+            )
+
+        return queries
+
+
+nums = list(map(int, input("Enter nums (space separated): ").split()))
+queries = list(map(int, input("Enter queries (space separated): ").split()))
+
+obj = Solution()
+result = obj.solveQueries(nums, queries)
+
+print("Output:", result)
+
+# 1 2 1 3 2 1
+# 0 2 4
+# -> [1, 2, 3]
